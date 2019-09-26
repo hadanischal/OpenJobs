31ccfbe95a464b68936dacfb66eef3714195362d
Nischal Hada <nischal@moroku.com>
2019-09-26 20:51:53 +1000

    Initial Commit

---
+++ OpenJobsTests/OpenJobsTests.swift
@@ -0,0 +1,34 @@
+//
+//  OpenJobsTests.swift
+//  OpenJobsTests
+//
+//  Created by Nischal Hada on 26/9/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+
+import XCTest
+@testable import OpenJobs
+
+class OpenJobsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
