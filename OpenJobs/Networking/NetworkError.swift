4d12c3da7cd2536840d093347750a6ea13b326b7
Nischal Hada <hadanischal@gmail.com>
2019-10-08 14:35:35 +1100

    Bugfixes for cellForRowAtindex path disposeBag issue

---
+++ OpenJobs/Networking/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  OpenJobs
+//
+//  Created by Nischal Hada on 6/10/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+}
