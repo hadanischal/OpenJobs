2d0431b337f2772c7b7c6810f110995cf95ca339
Nischal Hada <hadanischal@gmail.com>
2020-07-26 13:13:24 +1000

    [NetworkLayer] Added core NetworkLayer

---
+++ OpenJobs/NetworkLayer/Core/WebService.swift
@@ -0,0 +1,19 @@
+//
+//  WebService.swift
+//  OpenJobs
+//
+//  Created by Nischal Hada on 26/9/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+
+import RxSwift
+
+protocol WebServiceProtocol {
+    func load<T: Decodable>(resource: Resource<T>) -> Observable<T>
+}
+
+final class WebService: WebServiceProtocol {
+    func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        return URLRequest.load(resource: resource)
+    }
+}
