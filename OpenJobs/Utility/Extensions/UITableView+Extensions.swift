c90e1c6a3f579051b2b2ac9c0b5d1808a072be91
Nischal Hada <hadanischal@gmail.com>
2020-08-01 09:13:15 +1000

    [UITableView Extensions ] Added UITableView+Extension

---
+++ OpenJobs/Utility/Extensions/UITableView+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UITableView+Extensions.swift
+//  OpenJobs
+//
+//  Created by Nischal Hada on 27/9/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+
+        return cell
+    }
+}
