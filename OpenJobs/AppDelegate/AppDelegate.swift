7058c26033b6f0b768c9a7c6f09b9ddf92262a42
Nischal Hada <hadanischal@gmail.com>
2020-08-02 10:48:19 +1000

    [Clean Up] Code cleanup and removed unused files

---
+++ OpenJobs/AppDelegate/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  OpenJobs
+//
+//  Created by Nischal Hada on 26/9/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+// swiftlint:disable all
+
+import CocoaLumberjack
+import CoreData
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupLoggers()
+        CoreDataStack.sharedInstance.applicationDocumentsDirectory()
+        return true
+    }
+
+    /**
+     * Set up loggers
+     */
+    private func setupLoggers() {
+        DDLog.add(DDOSLogger.sharedInstance) // Uses os_log
+
+        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
+        fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.add(fileLogger)
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        CoreDataStack.sharedInstance.saveContext()
+    }
+}
