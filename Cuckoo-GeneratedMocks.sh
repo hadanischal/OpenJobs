08cdcad23b11b65c9f2c67eded327ef8f42a88a2
Nischal Hada <hadanischal@gmail.com>
2020-08-02 10:46:34 +1000

    [BuildScripts] Added BuildScripts of swiftlint, Cuckoo, swiftgen and test.sh

---
+++ Cuckoo-GeneratedMocks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Define output file. Change "$PROJECT_DIR/Tests" to your test's root source folder, if it's not the default name.
+PROJECT_DIR="."
+PODS_ROOT="$PROJECT_DIR/Pods"
+OUTPUT_FILE="$PROJECT_DIR/OpenJobsTests/GeneratedMocks.swift"
+echo "Generated Mocks File = $OUTPUT_FILE"
+
+# Define input directory. Change "$PROJECT_DIR" to your project's root source folder, if it's not the default name.
+INPUT_DIR="$PROJECT_DIR/OpenJobs"
+echo "Mocks Input Directory = $INPUT_DIR"
+
+# Generate mock files, include as many input files as you'd like to create mocks for.
+${PODS_ROOT}/Cuckoo/run generate --testable "OpenJobs" \
+--output "${OUTPUT_FILE}" \
+"${INPUT_DIR}/NetworkLayer/Core/WebService.swift" \
+"${INPUT_DIR}/NetworkLayer/Services/JobsNetworkHandler.swift" \
+"${INPUT_DIR}/CoreDataManager/CoreDataManagerHandler.swift" \
+"${INPUT_DIR}/ViewModel/JobsListRepository.swift"
+
+# ... and so forth
